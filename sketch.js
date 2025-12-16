bb2af00616b19cc3c7f7974c068e5d32fabd0ece
Roselith Ash <roselithash@gmail.com>
2025-12-16 20:31:16 +0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,42 +1,29 @@
-let colors = [];
+let colors = []; // Array to store watercolor shades
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
-    noLoop(); // Draw once; we'll add subtle animation for glare later
-    
+    background(255); // Start with white canvas
+
     // Generate a palette of random watercolor-like colors
     for (let i = 0; i < 5; i++) {
-        colors.push(color(random(100, 255), random(100, 255), random(100, 255), 50));
-    }
-
-    drawWatercolor();
-}
-
-function drawWatercolor() {
-    background(255); // white canvas
-    
-    for (let i = 0; i < 100; i++) { // number of strokes
-        let c = random(colors);
-        noStroke();
-        fill(c);
-        // random ellipse strokes to mimic watercolor blobs
-        ellipse(random(width), random(height), random(100, 300), random(50, 200));
-    }
-    
-    drawGlare();
-}
-
-function drawGlare() {
-    // subtle glare effect
-    for (let i = 0; i < 50; i++) {
-        let x = random(width);
-        let y = random(height);
-        fill(255, 255, 255, random(5, 20));
-        ellipse(x, y, random(50, 150), random(20, 100));
+        colors.push(color(random(100, 255), random(100, 255), random(150, 255), 30));
     }
 }
 
+function draw() {
+    // If mouse is pressed, draw strokes following the mouse
+    if (mouseIsPressed) {
+        for (let i = 0; i < 5; i++) { // multiple layers for watercolor effect
+            let col = random(colors);
+            noStroke();
+            fill(col);
+            let size = random(20, 60);
+            ellipse(mouseX + random(-10, 10), mouseY + random(-10, 10), size, size);
+        }
+    }
+}
+
+// Optional: handle window resize
 function windowResized() {
     resizeCanvas(windowWidth, windowHeight);
-    drawWatercolor();
 }
